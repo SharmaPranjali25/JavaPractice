2284cc5a21caf4b25806a872408c88e595a25d3e
Pranjali Sharma <104714667+SharmaPranjali25@users.noreply.github.com>
2025-03-01 12:23:39 +0530

    new chnages (Priority)

--- Exp1Thread.java
+++ Exp1Thread.java
@@ -17,6 +17,25 @@ public class Exp1Thread extends Thread {
 		t3.setName("Debit");
 		
 		System.out.println(t1.getName()+" "+t2.getName()+" "+ t3.getName());
+		
+		System.out.println(MAX_PRIORITY);
+		System.out.println(NORM_PRIORITY);
+		System.out.println(MIN_PRIORITY);
+		
+		System.out.println(t1.getPriority());
+		System.out.println(t2.getPriority());
+		System.out.println(t3.getPriority());
+		
+		t1.setPriority(MAX_PRIORITY);
+		System.out.println(t1.getPriority());
+		
+		t1.setDaemon(true);
+		System.out.println(t1.isDaemon());
+		
+		ThreadGroup tg1= new ThreadGroup("Bank");
+		
+		
+		
 	}
 
 }
