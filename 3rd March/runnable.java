6f804bf3d008e77defdd458df2b2b75a3d898269
Pranjali Sharma <104714667+SharmaPranjali25@users.noreply.github.com>
2025-03-03 10:17:58 +0530

    Thread + Runnable Objects and Conversions

---
+++ 3rd March/runnable.java
@@ -0,0 +1,23 @@
+package com.ths;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+class RunnableImpl implements Runnable{
+	public void run() {
+		System.out.println("run() from a different thread than Main");
+	}
+}
+public class runnable {
+	static ExecutorService executor= Executors.newFixedThreadPool(2);
+	public static void main(String[] args) {
+		
+		
+		//create and run runnable task using thread class
+		RunnableImpl task= new RunnableImpl();
+		Thread thread= new Thread(task);
+		thread.start();
+		//create and run runnable task using Executor Service
+		executor.submit(task);
+		executor.shutdown();
+	}
+
+}
