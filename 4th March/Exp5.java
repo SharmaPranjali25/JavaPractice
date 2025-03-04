925a112008a5ba51d62af91378e4b8e56886cab8
Pranjali Sharma <104714667+SharmaPranjali25@users.noreply.github.com>
2025-03-04 15:51:57 +0530

    List->map (10 ques) Exp5.java

--- 4th March/Exp5.java
+++ 4th March/Exp5.java
@@ -7,7 +7,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
-
+//list->Map
 class Employee{
 	private int id;
 	private String name;
