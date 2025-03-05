e32bd4289eeb10a4929ce2ae3b6b52fbb619a9dc
Pranjali Sharma <104714667+SharmaPranjali25@users.noreply.github.com>
2025-03-05 12:32:43 +0530

    Update Exp1.java

--- 5th March/Exp1.java
+++ 5th March/Exp1.java
@@ -3,12 +3,19 @@ package com.fls;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Exp1 {
 public static void main(String[] args) {
 	Path path= Paths.get("src/bytetest.txt");
+
 	try {
+		String lines="";
+		Files.writeString(path, lines, StandardOpenOption.CREATE);
+		List<String> obj= new ArrayList<>();
+		Files.write(path, obj);
 		byte[] bs= Files.readAllBytes(path);
 		List<String> linesData= Files.readAllLines(path);
 		System.out.println("Reading byte data: "+ new String(bs));
@@ -18,4 +25,6 @@ public static void main(String[] args) {
 		e.printStackTrace();
 	}
 }
+
 }
+
