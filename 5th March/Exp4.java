217798789ba869f9ecc9790391337ada7dd6f4b5
Pranjali Sharma <104714667+SharmaPranjali25@users.noreply.github.com>
2025-03-05 12:22:33 +0530

    Update(Read Permissios) Exp4.java

--- 5th March/Exp4.java
+++ 5th March/Exp4.java
@@ -3,7 +3,10 @@ package com.fls;
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.nio.file.attribute.PosixFilePermission;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Stream;
 
 public class Exp4 {
@@ -15,6 +18,11 @@ public class Exp4 {
 		data= strm.filter(str->str.startsWith("T"))
 				.map(String:: toUpperCase).toList();
 		System.out.println(data);
+		Set<PosixFilePermission> perm= new HashSet<>();
+		perm.add(PosixFilePermission.OWNER_WRITE);
+		perm.add(PosixFilePermission.OWNER_READ);
+		perm.add(PosixFilePermission.GROUP_READ);
+		perm.add(PosixFilePermission.GROUP_WRITE);
 			
 		}
 		catch(Exception e) {
@@ -22,4 +30,5 @@ public class Exp4 {
 		}
 	}
 }
+
 //ouput: [THIS IS SIMPLE BYTE STREAM WRITE AND READ EXAMPLE]
