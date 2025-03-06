d5fa72c17c144da272dd7f77a44c3930d7c7c45c
Pranjali Sharma <104714667+SharmaPranjali25@users.noreply.github.com>
2025-03-06 12:44:32 +0530

    Update CalculatorTest1.java

--- Maeven06-03-2025/CalculatorTest1.java
+++ Maeven06-03-2025/CalculatorTest1.java
@@ -2,13 +2,27 @@ package com.test;
 
 import java.util.concurrent.TimeUnit;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Timeout;
+
+import org.junit.platform.suite.api.IncludeClassNamePatterns;
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.Suite;
+
+
+@Suite
+@DisplayName("Test Suit Example here")
+@SelectPackages({"com.test"})
+@IncludeClassNamePatterns(".*Test")
+@SelectClasses({CalculatorTest1.class, CalculatorTest.class})
 @Timeout(value=5, unit= TimeUnit.SECONDS)
 public class CalculatorTest1 {
 	Calculator obj;
 	@Test
 	@Timeout(value=3, unit= TimeUnit.SECONDS)
+	
 	public void testAdd() {
 		System.out.println("before sleep method");
 		try {
