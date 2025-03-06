98e59304c4e13311da4b584c43d0ee1564b7fba8
Pranjali Sharma <104714667+SharmaPranjali25@users.noreply.github.com>
2025-03-06 11:51:46 +0530

    important juint files

---
+++ Maeven06-03-2025/Calculator.java
@@ -0,0 +1,19 @@
+package com.test;
+//   src/main/java
+public class Calculator {
+public int add(int x, int y) { //x=10, y=30
+	return x+y;
+}
+
+public int sub(int x, int y) {
+	return x-y;
+}
+
+public int mul(int x, int y) {
+	return x*y;
+}
+
+public String greetUser(String msg) {
+	return msg;
+}
+}
---
+++ Maeven06-03-2025/CalculatorTest.java
@@ -0,0 +1,64 @@
+package com.test;
+import static org.junit.Assert.assertEquals;
+
+//  src/test/java
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class CalculatorTest {
+Calculator obj;
+static int x,y;
+
+@BeforeClass
+public static void beforeClass() {
+	System.out.println("Before all test cases only one time");
+	x=10;
+	y=50;
+}
+
+@AfterClass
+public static void AeforeClass() {
+	System.out.println("After all test cases only one time");
+	x=0;
+	y=0;
+}
+
+@Before
+public void setUp() {
+	System.out.println("Execute every test method before execution");
+}
+
+@After
+public void setDown() {
+	System.out.println("Execute every test after before execution");
+}
+@Test
+public void testAdd() {
+	System.out.println("testAdd method");
+	int result=obj.add(x, y);
+	assertEquals(60,result); //to check if we get same data or not
+	
+}
+@Test
+public void testSub() {
+	System.out.println("testSub method");
+	int result=obj.sub(x, y);
+	assertEquals(-40,result);
+	
+}
+@Test
+public void testMul() {
+	System.out.println("testMul method");
+	int result=obj.mul(x, y);
+	assertEquals(500,result);
+}
+@Test
+public void testGreetUser() {
+	System.out.println("testGreetUser method");
+	assertEquals("hi hello",obj.greetUser("hi hello"));
+	
+}
+}
