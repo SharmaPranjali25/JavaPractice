47645aee01985b75e70c57be329d9a53f796dda0
Pranjali Sharma <104714667+SharmaPranjali25@users.noreply.github.com>
2025-03-07 14:42:55 +0530

    Update Register.java

--- TomcatDay1/Registration/Register.java
+++ TomcatDay1/Registration/Register.java
@@ -16,46 +16,63 @@ import java.sql.PreparedStatement;
 public class Register extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-
-
+    
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.setContentType("text/html");
-		PrintWriter out= response.getWriter();
-		String name= request.getParameter("fname");
-		String uname= request.getParameter("user");
-		String pass= request.getParameter("pwd");
 		
-		//store name, uname , pass to database
-		String url= "jdbc:mysql://localhost:3306/assignment";
-		String userName= "root";
-		String password= "root@39";
+		response.setContentType("text/html");
+		
+		PrintWriter out = response.getWriter();
+		
+		String name = request.getParameter("fname");
+		String uname = request.getParameter("user");
+		String pass = request.getParameter("pwd");
+		
+		String url = "jdbc:mysql://localhost:3306/assignment";
+		String userName = "root";
+		String password = "root@39";
+		
 		try {
+			User obj= new User();
+			obj.setFullName(name);
+			obj.setUserName(uname);
+			obj.setPassword(pass);
+		
 			Class.forName("com.mysql.jdbc.Driver");
-			Connection con= DriverManager.getConnection(url, userName, password);
-			String sql= "insert into user_mla (mla_fname, mla_user, mla_pass)values(?,?,?)";
-			PreparedStatement pst= con.prepareStatement(sql);
+			Connection con = DriverManager.getConnection(url,userName,password);
 			
-			pst.setString(1, name);
-			pst.setString(2, uname);
-			pst.setString(3, pass);
+			String sql = "insert into user_mla (mla_fname, mla_user, mla_pass)values(?,?,?)";
 			
-			int x= pst.executeUpdate();
-			if(x>0) {
-				RequestDispatcher rd= request.getRequestDispatcher("home.html");
-				rd.forward(request,  response);
+			PreparedStatement pst = con.prepareStatement(sql);
+			
+			pst.setString(1, obj.getFullName());
+			pst.setString(2, obj.getUserName());
+			pst.setString(3, obj.getPassword());
+			
+			int x = pst.executeUpdate();
+			
+			if(x > 0)
+			{
+				//RequestDispatcher rd = request.getRequestDispatcher("home.html");
+				
+				//rd.forward(request, response);
+				response.sendRedirect("home.html");
 			}
-			else {
-				out.println("<center> <h3> <font color='red'>Registration Failed. </h3> </center>");
-				RequestDispatcher rd= request.getRequestDispatcher("RegistrationForm.html");
+			else
+			{
+				out.println("<center> <h3><font color = 'red'> Registration failed.</font> </h3></center>");
+				
+				RequestDispatcher rd = request.getRequestDispatcher("register.html");
+				
 				rd.include(request, response);
 			}
-		}
-		catch(Exception e) {
+			
+		
+		}catch(Exception e)
+		{
 			e.printStackTrace();
 		}
-		
-		
 		
 	}
 
 }
+
