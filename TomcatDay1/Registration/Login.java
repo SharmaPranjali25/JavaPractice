c7692241e4152e2af93b4ca5802183c23c0e34ec
Pranjali Sharma <104714667+SharmaPranjali25@users.noreply.github.com>
2025-03-07 12:56:23 +0530

    register

---
+++ TomcatDay1/Registration/Login.java
@@ -0,0 +1,73 @@
+package com.test;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+@WebServlet("/Login")
+public class Login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+   
+   
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		
+		response.setContentType("text/html");
+		PrintWriter out= response.getWriter();
+		
+		String name= request.getParameter("user");
+		String pass= request.getParameter("pwd");
+		
+		String driver= "com.mysql.jdbc.Driver";
+		String url= "jdbc:mysql://localhost:3306/assignment";
+		String userName= "root";
+		String password= "root@39";
+		
+		try {
+			Class.forName(driver);
+			Connection con= DriverManager.getConnection(url, userName, password);
+			String sql= "select mla_user, mla_pass from  user_mla where mla_user=? and mla_pass= ?";
+			PreparedStatement pst= con.prepareStatement(sql);
+			ResultSet rs= pst.executeQuery();
+			
+			pst.setString(1, name);
+			pst.setString(2, pass);
+			
+			int x= pst.executeUpdate();
+			if(x>0) {
+				RequestDispatcher rd= request.getRequestDispatcher("home.html");
+				rd.forward(request,  response);
+			}
+			else {
+				out.println("<center> <h3> <font color='red'>Login Failed. </h3> </center>");
+				RequestDispatcher rd= request.getRequestDispatcher("login.html");
+				rd.include(request, response);
+			}
+			
+			
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
+		
+		
+				
+		
+	
+		
+	
+
+
