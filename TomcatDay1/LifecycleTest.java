61326c585fd4135ff4dfa107b8d494561e6f26ac
Pranjali Sharma <104714667+SharmaPranjali25@users.noreply.github.com>
2025-03-07 11:15:10 +0530

    lifecycle

---
+++ TomcatDay1/LifecycleTest.java
@@ -0,0 +1,41 @@
+package com.test;
+
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+import java.io.IOException;
+//import javax.servlet.GenericServlet;
+//import javax.servlet.Servlet;
+
+@WebServlet("/LifecycleTest")
+public class LifecycleTest extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    
+     // Default constructor. 
+     
+    public LifecycleTest() {
+      
+    }
+
+	
+	 
+	public void init(ServletConfig config) throws ServletException {
+		System.out.println("init()");
+	}
+
+	
+	public void destroy() {
+		System.out.println("destroy()");
+	}
+
+
+	public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
+		System.out.println("processing request and response");
+	}
+
+}
